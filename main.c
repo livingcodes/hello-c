7c18d2a0d97aef7ac2e31824b42f2e07a21e0279
Casey Joyce <case.joyce@gmail.com>
2024-10-04 20:55:24 -0500

    hello world

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/* https://medium.com/@laura.derohan/compiling-c-files-with-gcc-step-by-step-8e78318052 */
+/* build in windows terminal ubuntu using gcc main.c */
+/* execute the created file "./a.out" to see output in shell */
+int main(void) {
+   puts("Hello C");
+   return (0);
+}
